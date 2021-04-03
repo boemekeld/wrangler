05836a249124f596bd55540cef5ca736ef33f56e
Acemir Sousa Mendes <acemirsm@gmail.com>
2021-04-03 20:21:47 +0100

    feature: add subset option to site settings

--- src/settings/toml/site.rs
+++ src/settings/toml/site.rs
@@ -16,6 +16,7 @@ pub struct Site {
     entry_point: Option<PathBuf>,
     pub include: Option<Vec<String>>,
     pub exclude: Option<Vec<String>>,
+    pub subset: Option<String>,
 }
 
 impl Site {
@@ -62,6 +63,7 @@ impl Default for Site {
             entry_point: Some(PathBuf::from(SITE_ENTRY_POINT)),
             include: None,
             exclude: None,
+            subset: None,
         }
     }
 }
